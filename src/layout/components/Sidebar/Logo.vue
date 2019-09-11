96b59fcca2344ac9cb5e8e9b9293736674f9e606
wuxiaofeng <wuxiaofeng@nat-ev.cn>
2019-09-11 20:19:58 +0800

    配置路由和菜单。

---
+++ src/layout/components/Sidebar/Logo.vue
@@ -0,0 +1,80 @@
+<template>
+  <div :class="{'collapse':collapse}" class="sidebar-logo-container">
+    <transition name="sidebarLogoFade">
+      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
+        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <h1 v-else class="sidebar-title">{{ title }} </h1>
+      </router-link>
+      <router-link v-else key="expand" class="sidebar-logo-link" to="/">
+        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <h1 class="sidebar-title">{{ title }} </h1>
+      </router-link>
+    </transition>
+  </div>
+</template>
+
+<script>
+import Logo from '@/assets/images/logo.png'
+
+export default {
+  name: 'SidebarLogo',
+  props: {
+    collapse: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      title: this.$t('indexPageName'),
+      logo: Logo
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebarLogoFade-enter-active {
+  transition: opacity 1.5s;
+}
+
+.sidebarLogoFade-enter,
+.sidebarLogoFade-leave-to {
+  opacity: 0;
+}
+
+.sidebar-logo-container {
+  position: relative;
+  width: 100%;
+  height: 84px;
+  line-height: 84px;
+  background: #2b2f3a;
+  text-align: center;
+  overflow: hidden;
+
+  & .sidebar-logo-link {
+    height: 100%;
+    width: 100%;
+    display: flex !important;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    & .sidebar-logo {
+      width: auto;
+      max-width: 80%;
+      max-height: 24px;
+    }
+
+    & .sidebar-title {
+      display: inline-block;
+      margin: 10px 0 0 0;
+      color: #fff;
+      font-weight: 600;
+      line-height: 1;
+      font-size: 16px;
+      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
+    }
+  }
+}
+</style>
